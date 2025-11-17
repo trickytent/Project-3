90d3fffa230a78c949c4f23e3a47f26f77644c8c
Ayaan Khatri <128146246+trickytent@users.noreply.github.com>
2025-11-17 16:53:47 +0530

    Text reveal animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <HeroSection />
+      <div className='h-dvh border border-red-500'></div>
 
     </main>
   )
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,12 +1,64 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 const HeroSection = () => {
-  return (
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create(".hero-title",{
+            type: "chars",
+        });
+
+        const tl = gsap.timeline({
+            delay: 1,
+        });
+
+        tl.to(".hero-content", {
+            opacity: 1,
+            y: 0,
+            ease: "power1.inOut"
+        }).to(".hero-text-scroll", {
+            duration: 1,
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100% , 0% 100%)",
+            ease: "circ.out"
+        }, "-=0.5")
+        .from(titleSplit.chars, {
+            yPercent: 200,
+            stagger: 0.02,
+            ease: "power2.out",
+        }, "-=0.5"
+        );
+
+        const heroTl = gsap.timeline({
+            scrollTrigger: {
+                trigger: ".hero-container",
+                start: "1% top",
+                end: "bottom top",
+                scrub: true,
+        },
+        });
+
+        heroTl.to(".hero-container", {
+            rotate: 7,
+            scale: 0.9,
+            yPercent: 30,
+            ease: "power1.inOut",
+
+        });
+
+
+
+    });
+
+    return (
     <section className="bg-main-bg">
         <div className="hero-container">
             <img src="/images/static-img.png" 
             alt="hero-image" 
             className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150 "
             />
-            <div className="hero-content">
+            <div className="hero-content opacity-0">
                 <div className="overflow-hidden">
                     <h1 className="hero-title">Freaking Delicious</h1>
                 </div>
@@ -30,7 +82,7 @@ const HeroSection = () => {
             </div>
         </div>
     </section>
-  );
+   );
 };
 
 export default HeroSection
