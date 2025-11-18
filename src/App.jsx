8fcb77a863cb42441034950287616aba804c21ee
Ayaan Khatri <128146246+trickytent@users.noreply.github.com>
2025-11-18 12:13:03 +0530

    scroll activated highlights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import MessageSection from './sections/MessageSection';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -11,6 +12,7 @@ const App = () => {
     <main>
       <NavBar />
       <HeroSection />
+      <MessageSection />
       <div className='h-dvh border border-red-500'></div>
 
     </main>
